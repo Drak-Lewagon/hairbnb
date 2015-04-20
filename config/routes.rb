d9efabf51eab96e4a22fee7a8e99bb6bacb0796e
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-04-20 16:36:01 +0200

    Added pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :flats
   end
 
   resources :flats, only: [:index, :show]
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root to: "pages#home"
 end
