10b3bf6a6938db8b82d53932bcb86b2f1686b6e4
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2015-04-20 16:26:29 +0200

    Added Controller Flat and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :firstname, presence: true
   validates :lastname,  presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email,     presence: true, uniqueness: true
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :flats
+  end
 
-
-
+  resources :flats, only: [:index, :show]
 end
